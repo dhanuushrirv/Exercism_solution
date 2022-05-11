8810b464664ebb59c8f14dcc8a10ea4f696b60e8
DHANUU SHRI R V <dhanuushrirv@DHANUUs-MacBook-Pro.local>
2022-05-11 22:15:13 +0530

    [Dhanuushri] Add. logic to implement if two ten rupees are given it should be the same

--- Rupee_Dojo2/src/main/java/com/thoughtworks/Rupee/Rupee.java
+++ Rupee_Dojo2/src/main/java/com/thoughtworks/Rupee/Rupee.java
@@ -1,9 +1,17 @@
 package com.thoughtworks.Rupee;
 
+import javax.management.relation.RelationSupport;
+
 public class Rupee {
     private final double value;
 
     public Rupee(double value) {
-        this.value=value;
+        this.value = value;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        Rupee newValue= (Rupee) obj;
+        return this.value == newValue.value;
     }
 }
--- Rupee_Dojo2/src/test/java/RupeeTest.java
+++ Rupee_Dojo2/src/test/java/RupeeTest.java
@@ -1,14 +1,24 @@
 import com.thoughtworks.Rupee.Rupee;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotSame;
 
 public class RupeeTest {
     @Test
     void shouldNotBeSameWhenTwoDifferentTenRupeesAreGiven() {
-        Rupee rupee = new Rupee(10);
-        Rupee anotherRupee = new Rupee(10);
+        Rupee tenRupee = new Rupee(10);
+        Rupee anotherTenRupee = new Rupee(10);
 
-        assertNotSame(rupee,anotherRupee);
+        assertNotSame(tenRupee,anotherTenRupee);
+    }
+
+    @Test
+    void shouldBeEqualWhenTwoDifferentTenRupeesAreGiven() {
+        Rupee tenRupee = new Rupee(10);
+        Rupee anotherTenRupee = new Rupee(10);
+
+        assertEquals(tenRupee,anotherTenRupee);
+
     }
 }
